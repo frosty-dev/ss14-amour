40412132165152cf5a6b566cf30a0cd47d36346a
deltanedas <39013340+deltanedas@users.noreply.github.com>
2024-05-26 05:14:29 +0000

    fix antag selection being evil (#28197)
    * fix antag selection being evil
    
    * fix test
    
    * untroll the other tests
    
    * remove role timer troll
    
    * Allow tests to modify antag preferences
    
    * Fix antag selection
    
    * Misc test fixes
    
    * Add AntagPreferenceTest
    
    * Fix lazy mistakes
    
    * Test cleanup
    
    * Try stop players in lobbies from being assigned mid-round antags
    
    * ranting
    
    * I am going insane
    
    ---------
    
    Co-authored-by: deltanedas <@deltanedas:kde.org>
    Co-authored-by: ElectroJr <leonsfriedrich@gmail.com>

--- Content.Server/Preferences/Managers/ServerPreferencesManager.cs
+++ Content.Server/Preferences/Managers/ServerPreferencesManager.cs
@@ -26,6 +26,7 @@ namespace Content.Server.Preferences.Managers
         [Dependency] private readonly IServerDbManager _db = default!;
         [Dependency] private readonly IPlayerManager _playerManager = default!;
         [Dependency] private readonly IDependencyCollection _dependencies = default!;
+        [Dependency] private readonly ILogManager _log = default!;
 
         // WD-EDIT
         [Dependency] private readonly SponsorsManager _sponsors = default!;
@@ -36,7 +37,9 @@ namespace Content.Server.Preferences.Managers
         private readonly Dictionary<NetUserId, PlayerPrefData> _cachedPlayerPrefs =
             new();
 
-        private readonly ISawmill _sawmill = default!;
+        private ISawmill _sawmill = default!;
+
+        private int MaxCharacterSlots => _cfg.GetCVar(CCVars.GameMaxCharacterSlots);
 
         public void Init()
         {
@@ -44,6 +47,7 @@ namespace Content.Server.Preferences.Managers
             _netManager.RegisterNetMessage<MsgSelectCharacter>(HandleSelectCharacterMessage);
             _netManager.RegisterNetMessage<MsgUpdateCharacter>(HandleUpdateCharacterMessage);
             _netManager.RegisterNetMessage<MsgDeleteCharacter>(HandleDeleteCharacterMessage);
+            _sawmill = _log.GetSawmill("prefs");
         }
 
         private async void HandleSelectCharacterMessage(MsgSelectCharacter message)
