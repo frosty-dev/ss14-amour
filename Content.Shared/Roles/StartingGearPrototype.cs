2b81bb34724e70d7258adeefd128d1b9801f3427
Jabak <163307958+Jabaks@users.noreply.github.com>
2024-06-26 14:15:36 +0300

    Loadut System. Component. Ptototype

--- Content.Shared/Roles/StartingGearPrototype.cs
+++ Content.Shared/Roles/StartingGearPrototype.cs
@@ -1,29 +1,14 @@
 using Content.Shared.Humanoid;
 using Content.Shared.Preferences;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array;
 
 namespace Content.Shared.Roles
 {
     [Prototype("startingGear")]
-    public sealed partial class StartingGearPrototype : IPrototype
+    public sealed partial class StartingGearPrototype : IPrototype, IInheritingPrototype
     {
-        [DataField]
-        public Dictionary<string, EntProtoId> Equipment = new();
 
-        /// <summary>
-        /// if empty, there is no skirt override - instead the uniform provided in equipment is added.
-        /// </summary>
-        [DataField]
-        public EntProtoId? InnerClothingSkirt;
-
-        [DataField]
-        public EntProtoId? Satchel;
-
-        [DataField]
-        public EntProtoId? Duffelbag;
-
-        [DataField]
-        public List<EntProtoId> Inhand = new(0);
         // White underwear
         [DataField("underweart")]
         private string _underweart = string.Empty;
@@ -32,28 +17,45 @@ namespace Content.Shared.Roles
         private string _underwearb = string.Empty;
         // White underwear end
 
+        /// <inheritdoc/>
         [ViewVariables]
         [IdDataField]
         public string ID { get; private set; } = string.Empty;
 
-        public string GetGear(string slot, HumanoidCharacterProfile? profile)
-        {
-            if (profile != null)
-            {
-                if (slot == "jumpsuit" && profile.Clothing == ClothingPreference.Jumpskirt && !string.IsNullOrEmpty(InnerClothingSkirt))
-                    return InnerClothingSkirt;
-                if (slot == "back" && profile.Backpack == BackpackPreference.Satchel && !string.IsNullOrEmpty(Satchel))
-                    return Satchel;
-                if (slot == "back" && profile.Backpack == BackpackPreference.Duffelbag && !string.IsNullOrEmpty(Duffelbag))
-                    return Duffelbag;
-                // White underwear
-                if (slot == "underweart" && profile.Sex == Sex.Female && !string.IsNullOrEmpty(_underweart))
-                    return _underweart;
-                if (slot == "underwearb" && profile.Sex == Sex.Female && !string.IsNullOrEmpty(_underwearb))
-                    return _underwearb;
-                // White underwear end
-            }
+        /// <inheritdoc/>
+        [ParentDataField(typeof(AbstractPrototypeIdArraySerializer<StartingGearPrototype>))]
+        public string[]? Parents { get; private set; }
 
+        /// <inheritdoc/>
+        [AbstractDataField]
+        public bool Abstract { get; }
+
+        /// <summary>
+        /// The slot and entity prototype ID of the equipment that is to be spawned and equipped onto the entity.
+        /// </summary>
+        [DataField]
+        [AlwaysPushInheritance]
+        public Dictionary<string, EntProtoId> Equipment = new();
+
+        /// <summary>
+        /// The inhand items that are equipped when this starting gear is equipped onto an entity.
+        /// </summary>
+        [DataField]
+        [AlwaysPushInheritance]
+        public List<EntProtoId> Inhand = new(0);
+
+        /// <summary>
+        /// Inserts entities into the specified slot's storage (if it does have storage).
+        /// </summary>
+        [DataField]
+        [AlwaysPushInheritance]
+        public Dictionary<string, List<EntProtoId>> Storage = new();
+
+        /// <summary>
+        /// Gets the entity prototype ID of a slot in this starting gear.
+        /// </summary>
+        public string GetGear(string slot)
+        {
             return Equipment.TryGetValue(slot, out var equipment) ? equipment : string.Empty;
         }
     }
