48c86bd8467fd10bbcf1f1f01b6934cffb888029
ThereDrD <88589686+ThereDrD0@users.noreply.github.com>
2024-08-08 19:57:49 +0300

    Новые амбиенты и пару песен (#586)
    * Фича
    
    * Доделываю
    
    * Sussy update # 1 (#45)
    
    * remove useless field's serialization
    
    * fix: use invariant culture where is possible
    
    * feat: rotatable radars
    
    * feat: menu ref
    
    * tweak: delete useless
    
    * fix
    
    * new
    
    * feat: new ambient system
    
    * add: new lobby audio files
    
    * feat: helmet overlays & HUDs
    
    * fix
    
    * fix
    
    * tweak: add a way to play without RTX
    
    * ooops
    
    * forget
    
    * Revert "forget"
    
    This reverts commit 244cdbe5562d759250697e7b65da2471dd809456.
    
    * privet ya valtos leniviy yeban
    
    * remove: drink delay
    
    * tweak: don't kick player after ban
    
    * fix: shaders
    
    * tweak: ban message
    
    * sus
    
    * oops
    
    Co-authored-by: Игорь Спичкин <55196698+igorsaux@users.noreply.github.com>
    
    * Revert "Sussy update # 1 (#45)"
    
    This reverts commit 810fa91adc76144527f9bb0e4e3f7ccca4ebe697.
    
    * holy shit so many sounds
    
    * proebalis
    
    ---------
    
    Co-authored-by: wCATw <leb.beliiii@gmail.com>
    Co-authored-by: rhailrake <49613070+rhailrake@users.noreply.github.com>
    Co-authored-by: Игорь Спичкин <55196698+igorsaux@users.noreply.github.com>

--- Content.Server/Body/Components/BloodstreamComponent.cs
+++ Content.Server/Body/Components/BloodstreamComponent.cs
@@ -104,7 +104,7 @@ namespace Content.Server.Body.Components
         ///     The sound to be played when a weapon instantly deals blood loss damage.
         /// </summary>
         [DataField]
-        public SoundSpecifier InstantBloodSound = new SoundCollectionSpecifier("blood");
+        public SoundSpecifier InstantBloodSound = new SoundCollectionSpecifier("BloodLossRun");
 
         /// <summary>
         ///     The sound to be played when some damage actually heals bleeding rather than starting it.
@@ -180,6 +180,12 @@ namespace Content.Server.Body.Components
         ///     Bool for bleeding alert.
         /// </summary>
         public bool IsBleeding => BleedAmount > 0;
+
+        public readonly AudioParams DefaultParams = AudioParams.Default.WithVolume(-1f);
+
+        public SoundSpecifier? BloodLossSound = new SoundCollectionSpecifier("BloodLoss");
+
+        //public SoundSpecifier? BloodLossRunSound = new SoundCollectionSpecifier("");
 
         //WD-EDIT
     }
