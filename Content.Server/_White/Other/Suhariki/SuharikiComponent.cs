7138d9bbdff2e910b249614cf8af0339ae538cd0
BIGZi0348 <118811750+bigzi0348@users.noreply.github.com>
2024-09-21 14:46:38 +0300

    Suhariki (#4)
    * init
    
    * Porting is complete
    
    Still WIP
    
    * small stuff
    
    * SmallStuff
    
    * awawawa
    
    * Нас рано
    
    * Engi
    
    * More luck
    
    * removed some shit
    
    (cherry picked from commit e1d745655b0e7253ea4e4814b82dd56e90153514)

---
+++ Content.Server/_White/Other/Suhariki/SuharikiComponent.cs
@@ -0,0 +1,49 @@
+using Robust.Shared.Prototypes;
+using Content.Shared.Damage;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Content.Shared.Chat.Prototypes;
+using Robust.Shared.Audio;
+// WD Engi Exclusive
+namespace Content.Server._White.Other.Suhariki;
+
+[RegisterComponent, Access(typeof(SuharikiSystem))]
+public sealed partial class SuharikiComponent : Component
+
+{
+    /// <summary>
+    /// Amount and type of damage that will be dealt on event.
+    /// </summary>
+    [DataField(required: true)]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public DamageSpecifier Damage = new();
+
+    /// <summary>
+    /// Chance of event activation.
+    /// </summary>
+    [DataField]
+    public float Chance = 0;
+
+    /// <summary>
+    /// Amount of rolls for event.
+    /// </summary>
+    [DataField]
+    public int StonesInFood = 1;
+
+    /// <summary>
+    /// The prototype that will be spawned on event.
+    /// </summary>
+    [DataField("holdingPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>)), ViewVariables(VVAccess.ReadWrite)]
+    public string HoldingPrototype = "SuharikiTooth";
+
+    /// <summary>
+    /// Emote triggered on event.
+    /// </summary>
+    [DataField("emote", customTypeSerializer: typeof(PrototypeIdSerializer<EmotePrototype>))]
+    public string EmoteId = "Scream";
+
+    /// <summary>
+    /// Sound triggered on event.
+    /// </summary>
+    [DataField]
+    public SoundSpecifier UseSound { get; set; } = new SoundPathSpecifier("/Audio/White/Object/Misc/Suhariki/tooth_break.ogg");
+}
