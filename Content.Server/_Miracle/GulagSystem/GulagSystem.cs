72571f1f8538fc92e6994199e0d2ccb144b4e7b7
Jabak <163307958+Jabaks@users.noreply.github.com>
2024-09-25 20:22:09 +0300

    То что возмжно починит сервер

--- Content.Server/_Miracle/GulagSystem/GulagSystem.cs
+++ Content.Server/_Miracle/GulagSystem/GulagSystem.cs
@@ -135,7 +135,7 @@ public sealed partial class GulagSystem : SharedGulagSystem
             return;
         }
 
-        if (ev.Primary is not KillPlayerSource source || ev.Suicide)
+        if (ev.Primary is not KillPlayerSource source)
         {
             return;
         }
