4ef857985cce11d9719a2adde28abdfbe8c3991f
Jabak <163307958+Jabaks@users.noreply.github.com>
2024-10-01 23:37:57 +0300

    govno edit

--- Content.Server/_Honk/RoundEndVote/RoundEndVoteSystem.cs
+++ Content.Server/_Honk/RoundEndVote/RoundEndVoteSystem.cs
@@ -12,6 +12,7 @@ public sealed class RoundEndVoteSystem : EntitySystem
 {
     [Dependency] private readonly IVoteManager _voteManager = default!;
     [Dependency] private readonly GameTicker _gameTicker = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
 
     public override void Initialize()
     {
@@ -20,10 +21,20 @@ public sealed class RoundEndVoteSystem : EntitySystem
 
     private void OnRoundEndSystemChange(RoundEndSystemChangedEvent ev)
     {
-        if(_gameTicker.RunLevel != GameRunLevel.PreRoundLobby)
+        if (_gameTicker.RunLevel != GameRunLevel.PreRoundLobby)
             return;
 
-        _voteManager.CreateStandardVote(null, StandardVoteType.Preset);
+        var player = _playerManager.PlayerCount >= 20;
+
+        if (player)
+        {
+            _voteManager.CreateStandardVote(null, StandardVoteType.Preset);
+        }
+        else
+        {
+            _gameTicker.SetGamePreset("Secret");
+        }
+
         _voteManager.CreateStandardVote(null, StandardVoteType.Map);
     }
 
