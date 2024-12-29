b62f465c4135b4c610d331dea15d4d1caa8b8fa1
Іван Оніщенко <onishchenko_is@ffeks.dnu.edu.ua>
2024-12-29 20:59:01 +0300

    ups

--- Content.Server/_White/AspectsSystem/Aspects/RandomAccentAspect.cs
+++ Content.Server/_White/AspectsSystem/Aspects/RandomAccentAspect.cs
@@ -44,6 +44,7 @@ public sealed class RandomAccentAspect : AspectSystem<RandomAccentAspectComponen
                 return;
 
             var mob = ev.Mob;
+            ApplyRandomAccent(mob);
             ChatHelper.SendAspectDescription(mob, Loc.GetString("random-accent-aspect-desc"));
         }
     }
