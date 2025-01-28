add7181afbaf79372e7aa9ddc1ba5c18515bca97
BIGZi0348 <svalker0348@gmail.com>
2025-01-28 20:24:17 +0300

    Revert "Merge pull request #253 from frosty-dev/CRAB17"
    This reverts commit 1aa050914b1450c180052141f83bf70b22533695, reversing
    changes made to f1f191270d0ae51c4743f422d85a8e15a6d013f6.

--- Content.Shared/_Amour/ProtocolCRAB17/SharedProtocolCRAB17System.cs
+++ Content.Shared/_Amour/ProtocolCRAB17/SharedProtocolCRAB17System.cs
@@ -1,6 +1,5 @@
 using Content.Shared._White.Economy;
 using Content.Shared.DoAfter;
-using Content.Shared.Examine;
 using Content.Shared.Interaction;
 using Content.Shared.Interaction.Events;
 using Content.Shared.Popups;
@@ -23,7 +22,6 @@ public abstract class SharedProtocolCRAB17System : EntitySystem
         SubscribeLocalEvent<ProtocolCRAB17Component, UseInHandEvent>(OnUseInHand);
         SubscribeLocalEvent<ProtocolCRAB17Component, ProtocolCRAB17DoAfterEvent>(OnDoAfter);
         SubscribeLocalEvent<ProtocolCRAB17Component, InteractUsingEvent>(OnInteractUsing);
-        SubscribeLocalEvent<ProtocolCRAB17Component, ExaminedEvent>(OnExamine);
     }
 
     private void OnInteractUsing(EntityUid uid, ProtocolCRAB17Component component, InteractUsingEvent args)
@@ -66,20 +64,6 @@ public abstract class SharedProtocolCRAB17System : EntitySystem
         _popupSystem.PopupClient(Loc.GetString("protocol-CRAB17-try-activate"), uid, args.User);
         _doAfterSystem.TryStartDoAfter(doAfterEventArgs);
         args.Handled = true;
-    }
-
-    private void OnExamine(EntityUid uid, ProtocolCRAB17Component component, ExaminedEvent args)
-    {
-        if (!TryComp<ProtocolCRAB17Component>(uid, out var comp))
-            return;
-
-        if (!args.IsInDetailsRange)
-            return;
-
-        string bankID = component.BankAccountId != null ? ((int) component.BankAccountId).ToString() : "отсутствует";
-
-        var message = Loc.GetString("protocol-CRAB17-cardID", ("item", bankID));
-        args.PushMarkup(message);
     }
 
     public abstract void OnDoAfter(Entity<ProtocolCRAB17Component> ent, ref ProtocolCRAB17DoAfterEvent args);
