773dba359c75ad88007732215ec1d444f8872351
withoutcode333 <66198468+withoutcode333@users.noreply.github.com>
2025-04-13 17:32:46 +0500

    [Feature] Executions (#932)
    * based
    
    * cleanup
    
    * cleanup + fixes
    
    * fix
    
    * fix
    
    * fix ftl
    
    * Update Resources/Locale/ru-RU/_white/executions/execution.ftl
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update execution.ftl
    
    * правки
    
    * vrode norm
    
    * da
    
    ---------
    
    Co-authored-by: BIGZi0348 <118811750+BIGZi0348@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: BIGZi0348 <svalker0348@gmail.com>
    (cherry picked from commit 83e164172f8e290acee7634f14ac51281be020ad)

---
+++ Content.Shared/_White/Executions/ExecutionComponent.cs
@@ -0,0 +1,16 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._White.Executions;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ExecutionComponent : Component
+{
+    [DataField]
+    public bool Enabled = true;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan NextAttempt = TimeSpan.FromSeconds(3);
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public TimeSpan NextUse = TimeSpan.Zero;
+}
